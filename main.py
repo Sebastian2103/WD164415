ed9a3ceecb34ae51c1dbb994845765484b05d910
Sebastian Pająk <42960067+Sebastian2103@users.noreply.github.com>
2022-03-02 13:27:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,88 @@
 #Zadanie1
-# a='Witam'
-# b="Koncepcja"
-# c= 21
-# d=24121
-# e=21.12
-# f=12.891
-# g=12+44j
-# h=12+41j
-# print(a,b,c,d,e,f,g,h,)
+a='Witam'
+b="Koncepcja"
+c= 21
+d=24121
+e=21.12
+f=12.891
+g=12+44j
+h=12+41j
+print(a,b,c,d,e,f,g,h,)
 
 #zadanie2
-# a=7
-# b=3
-# c=12
-# dzielenie=a/c
-# dodawanie=a+c
-# odejmowanie=c-b
-# mnozenie=a*c
-# reszta=c%b
-# dzielenie_calkowite=a//b
-# potega=a**b
-# print(dzielenie)
-# print(dodawanie)
-# print(odejmowanie)
-# print(mnozenie)
-# print(reszta)
-# print(dzielenie_calkowite)
-# print(potega)
+a=7
+b=3
+c=12
+dzielenie=a/c
+dodawanie=a+c
+odejmowanie=c-b
+mnozenie=a*c
+reszta=c%b
+dzielenie_calkowite=a//b
+potega=a**b
+print(dzielenie)
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(reszta)
+print(dzielenie_calkowite)
+print(potega)
 
 #zadanie3
-# a=5
-# a+=1
-# print(a)
-# b=5
-# b-=1
-# print(b)
-# c=5
-# c*=2
-# print(c)
-# d=5
-# d/=2
-# print(d)
-# e=5
-# e**=2
-# print(e)
-# f=5
-# f%=2
-# print(f)
+a=5
+a+=1
+print(a)
+b=5
+b-=1
+print(b)
+c=5
+c*=2
+print(c)
+d=5
+d/=2
+print(d)
+e=5
+e**=2
+print(e)
+f=5
+f%=2
+print(f)
 
 #zadanie4
-# e=2
-# e**=10
-# print(e)
+imoprt math
+
+z1= math.e**10
+print(z1)
+
+z2=math.pow(math.log(5 + math.pow(math.sin(8),2)),1/6)
+print(z2)
+
+z3=math.floor(3.5)
+print(z3)
 
 #zadanie5
-# a="SEBASTIAN"
-# s2='PAJAK'
-# print(a.capitalize(),s2.capitalize())
+a="SEBASTIAN"
+s2='PAJAK'
+print(a.capitalize(),s2.capitalize())
 
 #zadanie6
 
-# c='la la la'
-# print(c.count('la'))
+c='la la la'
+print(c.count('la'))
 
 #zadanie7
-# imie='sebastian'
-# print(imie[1],imie[8])
+imie='sebastian'
+print(imie[1],imie[8])
 
 #zadanie8
 
-# c='la\n la\n la'
-# print(c.split())
+c='la\n la\n la'
+print(c.split())
 
 #zadanie9
-# a='imie'
-# b=2.5
-# c=hex(12)
-# print(a,b,c)
+a='imie'
+b=2.5
+c=hex(12)
+print(a,b,c)
 
 
