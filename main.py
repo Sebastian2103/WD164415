71d4162f869c26bfb3aacae0812fd6a4f3a87f25
Sebastian Pająk <42960067+Sebastian2103@users.noreply.github.com>
2022-10-07 12:52:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# #Zadanie1
+# def foo(a, y):
+#     return print(a,".", y)
+# a="J"
+# y="Kowalski"
+# foo(a,y)
+
+
+# #Zadanie2
+# def foo(imie, nazwisko):
+#     return print (imie[0].title(),".",nazwisko.title())
+# imie = "jan"
+# nazwisko = "Nowak"
+# foo(imie,nazwisko)
+
+#Zadanie3
+def foo(dwie_p_cyfry,dwie_ost_cyfry,wiek):
+    if wiek<dwie_ost_cyfry:
+        return print(dwie_p_cyfry,(dwie_ost_cyfry-wiek))
+a=20
+b=22
+c=14
+foo(a,b,c)
+#  #Zadanie5
+# def foo(a,b):
+#     if(a,b>0 and b!=0):
+#         return a/b
+# a=12
+# b=4
+# print(foo(a,b))
